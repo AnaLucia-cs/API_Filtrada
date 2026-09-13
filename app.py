fcbd83f165c35b246bc0d93a04aabb050438e976
Diego Francisco Martinez Reyes <diegofcomtzreyes@gmail.com>
2026-09-12 22:56:46 -0600

    Fix app.run() call to include host and port

--- app.py
+++ app.py
@@ -610,6 +610,7 @@ def obtener_pedido_aceptado():
 if __name__ == "__main__":
     # Cargar las condiciones que ya existían
     # en pedido.json al arrancar Flask.
+    app.run(host='0.0.0.0', port=5000, debug=True)
     cargar_condiciones()
     app.run(
         debug=True
